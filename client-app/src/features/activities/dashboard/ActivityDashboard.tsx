e7c6a2e309a12940b1076a32e28fde6021443216
amjad arabia <black_street@hotmail.co.il>
2022-09-13 13:03:18 +0300

    End of Section 9

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from "react";
 import { Grid} from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../app/stores/stores";
+import ActivityFilters from "./ActivityFilters";
 import ActivityList from "./ActivityList";
 
 
@@ -26,7 +27,7 @@ export default observer (function ActivityDashboard(){
                 <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
-               
+               <ActivityFilters/>
             </Grid.Column>
         </Grid>
     )
