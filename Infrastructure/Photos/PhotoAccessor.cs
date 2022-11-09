5472c3aed0087cda8d08bbb50575988ace668511
amjad arabia <black_street@hotmail.co.il>
2022-11-09 15:09:07 +0200

    start sec 15

---
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -0,0 +1,25 @@
+using Application.Interfaces;
+using Application.Photos;
+using CloudinaryDotNet;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Photos
+{
+    public class PhotoAccessor : IPhotoAccessor
+    {
+        private readonly Cloudinary _cloudinary;
+        public PhotoAccessor(IOptions<CloudinarySettings> config)
+        {
+
+        }
+        public Task<PhotoUploadResult> AddPhoto(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DeletePhoto(string publicId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
